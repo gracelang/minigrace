c4343a63ec1508a1fcff11f2cddb2006a15f6c1a
Michael Homer <mwh@ecs.vuw.ac.nz>
2011-06-23 11:54:05 +1200

    Add comments to unicode.c

--- unicode.c
+++ unicode.c
@@ -3,6 +3,35 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+// unicode.c defines a Grace module "unicode", which can be compiled either
+// to LLVM bitcode (called unicode.gco) and linked statically, or to
+// native code (called unicode.gso) and linked dynamically. In either case
+// it is imported with "import unicode" and thereafter available as "unicode".
+// The module contains these methods:
+//   category(char : String) -> String
+//   bidirectional(char : String) -> String
+//   combining(char : String) -> Number
+//   mirrored(char : String) -> Boolean
+//   name(char : String) -> String
+//   iscategory(char : String, category : String) -> Boolean
+//   isSeparator(char : String) -> Boolean
+//   isControl(char : String) -> Boolean
+//   isLetter(char : String) -> Boolean
+//   isNumber(char : String) -> Boolean
+//   isSymbolMathematical(char : String) -> Boolean
+//   create(codepoint : Number) -> String
+// These all return or test the corresponding Unicode property, or in the
+// case of create return a String consisting of the character with the given
+// codepoint.
+//
+// The character database is in unicodedata.h, which was automatically
+// generated from the UnicodeData.txt file available from unicode.org. The
+// data in that file is structured as a simple lookup table for codepoints
+// below 0x20000 (the Basic Multilingual Plane and the Supplementary
+// Multilingual Plane), compiled into the executable.
+
+// The necessary headers for interacting with the rest of the system
+// (should be an actual header)
 struct Method {
     char *name;
     struct Object* (*func)(struct Object*, int,
@@ -52,8 +81,12 @@ struct Object *alloc_obj();
 char *cstringfromString(struct Object*);
 int integerfromAny(struct Object*);
 
+// Intern module
 struct Object *unicode_module = NULL;
 
+// Return a Grace String containing the Unicode name of the first 
+// character in the String argument (e.g.,
+// "LATIN SMALL LETTER A WITH DIARESIS").
 struct Object *unicode_name(struct Object *self, int nparams,
          struct Object **args) {
     struct Object *o = callmethod(args[0], "ord", 0, 0);
@@ -61,6 +94,8 @@ struct Object *unicode_name(struct Object *self, int nparams,
     const char *name = Unicode_Names[v];
     return alloc_String(name);
 }
+// Return a Grace string containing the Unicode category of the first
+// character in the String argument (e.g., "Nd").
 struct Object *unicode_category(struct Object *self, int nparams,
          struct Object **args) {
     struct Object *o = callmethod(args[0], "ord", 0, 0);
@@ -68,6 +103,8 @@ struct Object *unicode_category(struct Object *self, int nparams,
     int cindex = UnicodeRecords[v].category;
     return alloc_String(Unicode_Categories[cindex]);
 }
+// Return a Grace Number containing the Unicode combining class of
+// the first character in the String argument (e.g. 10).
 struct Object *unicode_combining(struct Object *self, int nparams,
          struct Object **args) {
     struct Object *o = callmethod(args[0], "ord", 0, 0);
@@ -75,6 +112,8 @@ struct Object *unicode_combining(struct Object *self, int nparams,
     int cval = UnicodeRecords[v].combining;
     return alloc_Float64(cval);
 }
+// Return a Grace Boolean indicating whether the first character in
+// the argument String is marked as mirrored in the Unicode database.
 struct Object *unicode_mirrored(struct Object *self, int nparams,
          struct Object **args) {
     struct Object *o = callmethod(args[0], "ord", 0, 0);
@@ -82,6 +121,8 @@ struct Object *unicode_mirrored(struct Object *self, int nparams,
     int cval = UnicodeRecords[v].mirrored;
     return alloc_Boolean(cval == 'Y');
 }
+// Return a Grace String indicating the bidirectionality of the first
+// character in the argument String as provided in the Unicode.
 struct Object *unicode_bidirectional(struct Object *self, int nparams,
          struct Object **args) {
     struct Object *o = callmethod(args[0], "ord", 0, 0);
@@ -89,6 +130,10 @@ struct Object *unicode_bidirectional(struct Object *self, int nparams,
     int cindex = UnicodeRecords[v].bidirectional;
     return alloc_String(Unicode_Bidirectionals[cindex]);
 }
+// Return a Grace Boolean indicating whether the first character of the first
+// argument String is in the Unicode category given by the second. The second
+// can be either one or two characters in length, testing for either a broad
+// category (like "N") or a specific one (like "Nd").
 struct Object *unicode_iscategory(struct Object *self, int nparams,
         struct Object **args) {
     struct Object *o = callmethod(args[0], "ord", 0, 0);
@@ -107,6 +152,7 @@ struct Object *unicode_iscategory(struct Object *self, int nparams,
     free(dt);
     return ret;
 }
+// Return a Grace Boolean indicating whether the argument is a separator.
 struct Object *unicode_isSeparator(struct Object *self, int nparams,
         struct Object **args) {
     struct Object *o = args[0];
@@ -118,6 +164,8 @@ struct Object *unicode_isSeparator(struct Object *self, int nparams,
     const char *cat = Unicode_Categories[cindex];
     return alloc_Boolean(cat[0] == 'Z');
 }
+// Return a Grace Boolean indicating whether the argument is a control
+// character.
 struct Object *unicode_isControl(struct Object *self, int nparams,
         struct Object **args) {
     struct Object *o = args[0];
@@ -129,6 +177,7 @@ struct Object *unicode_isControl(struct Object *self, int nparams,
     const char *cat = Unicode_Categories[cindex];
     return alloc_Boolean(cat[0] == 'C');
 }
+// Return a Grace Boolean indicating whether the argument is a letter.
 struct Object *unicode_isLetter(struct Object *self, int nparams,
         struct Object **args) {
     struct Object *o = args[0];
@@ -140,6 +189,7 @@ struct Object *unicode_isLetter(struct Object *self, int nparams,
     const char *cat = Unicode_Categories[cindex];
     return alloc_Boolean(cat[0] == 'L');
 }
+// Return a Grace Boolean indicating whether the argument is a Number.
 struct Object *unicode_isNumber(struct Object *self, int nparams,
         struct Object **args) {
     struct Object *o = args[0];
@@ -151,6 +201,8 @@ struct Object *unicode_isNumber(struct Object *self, int nparams,
     const char *cat = Unicode_Categories[cindex];
     return alloc_Boolean(cat[0] == 'N');
 }
+// Return a Grace Boolean indicating whether the argument is a
+// Symbol, Mathematical.
 struct Object *unicode_isSymbolMathematical(struct Object *self, int nparams,
         struct Object **args) {
     struct Object *o = args[0];
@@ -162,6 +214,10 @@ struct Object *unicode_isSymbolMathematical(struct Object *self, int nparams,
     const char *cat = Unicode_Categories[cindex];
     return alloc_Boolean(strcmp(cat, "Sm") == 0);
 }
+// Return a Grace string of length 1 containing the codepoint given by
+// the Number argument. Strings are stored in UTF-8, so the function
+// constructs a buffer and fills it with the appropriate bytes, then
+// calls the String constructur with it.
 struct Object *unicode_create(struct Object *self, int nparams,
          struct Object **args) {
     struct Object *o = args[0];
@@ -212,6 +268,7 @@ struct Object *unicode_create(struct Object *self, int nparams,
     }
     return alloc_String((char*)buf);
 }
+// Create and return a Grace object with all the above functions as methods.
 struct Object *module_unicode_init() {
     if (unicode_module != NULL)
         return unicode_module;
