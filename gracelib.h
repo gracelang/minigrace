7c2973d693af781d50a453b8dd5ec84749b4a684
Michael Homer <mwh@ecs.vuw.ac.nz>
2012-08-03 19:20:03 +1200

    Add more code gen prototypes to header

--- gracelib.h
+++ gracelib.h
@@ -136,6 +136,8 @@ void gracelib_argv(char **);
 void module_sys_init_argv(Object);
 void gracelib_stats();
 void addtoclosure(Object, Object *);
+void glfree(void *);
+void setCompilerModulePath(char *);
 Object *getfromclosure(Object, int);
 void addmethod2(Object, char *, Object (*)(Object, int, int*, Object*, int));
 Method *addmethod2pos(Object, char *, Object (*)(Object, int, int*, Object*, int), int);
