09134dc59fd731c244516805e1cd33df4087ab5e
Michael Homer <mwh@ecs.vuw.ac.nz>
2013-03-06 19:28:59 +1300

    Allow loading samples in JS UI
    A new dropdown and button, similar to the test case system, permits the
    user to select one of the code samples (currently all dialects) and load
    them up. When they load a module depending on another (like a dialect),
    a precompiled version of that dependency is fetched and loaded if the
    module has not already been compiled. GCT files are also fetched and
    stored in the cache.

---
+++ js/samples.js
@@ -0,0 +1,84 @@
+var samples = {
+    'fsm': {
+        'name': 'Finite state machine dialect',
+        'dir': 'dialects',
+    },
+    'fsm_example': {
+        'name': 'FSM client code',
+        'requires': ['fsm'],
+        'dir': 'dialects',
+    },
+    'ObjectAssociations': {
+        'name': 'Object associations dialect',
+        'dir': 'dialects',
+    },
+    'ObjectAssociations_example': {
+        'name': 'OA client code',
+        'dir': 'dialects',
+        'requires': ['ObjectAssociations'],
+    },
+    'StaticGrace': {
+        'name': 'Static Grace dialect',
+        'dir': 'dialects',
+    },
+    'dialect': {
+        'name': 'Dialect dialect',
+        'dir': 'dialects',
+    },
+    'dialect_example': {
+        'name': 'Dialect client code',
+        'dir': 'dialects',
+        'requires': ['dialect'],
+    },
+};
+
+var sm = document.getElementById('sample');
+for (var s in samples) {
+    var opt = document.createElement('option');
+    opt.value = s;
+    opt.innerHTML = samples[s].name;
+    sm.appendChild(opt);
+}
+
+function loadSampleJS(k) {
+    if (window[k])
+        return;
+    var sample = samples[k];
+    var req = new XMLHttpRequest();
+    req.open("GET", "./sample/" + sample.dir + '/' + k + ".js", false);
+    req.send(null);
+    if (req.status == 200) {
+        var theModule;
+        eval(req.responseText);
+        eval("theModule = gracecode_" + k + ";");
+        window['gracecode_' + k] = theModule;
+    } else {
+        alert("Loading sample JavaScript code failed: retrieving " + k +
+                " returned " + req.status);
+    }
+    req.open("GET", "./sample/" + sample.dir + '/' + k + ".gct", false);
+    req.send(null);
+    if (req.status == 200) {
+        gctCache[k] = req.responseText;
+    } else {
+        alert("Loading sample JavaScript code metadata failed: retrieving "
+                + k + " returned " + req.status);
+    }
+}
+
+function loadsample(k) {
+    var sample = samples[k];
+    if (sample.requires) {
+        for (var i=0; i<sample.requires.length; i++)
+            loadSampleJS(sample.requires[i]);
+    }
+    var req = new XMLHttpRequest();
+    req.open("GET", "./sample/" + sample.dir + '/' + k + ".grace", false);
+    req.send(null);
+    if (req.status == 200) {
+        if (ace)
+            editor.setValue(req.responseText);
+        document.getElementById("code_txt").value = req.responseText;
+        document.getElementById('modname').value = k;
+    }
+}
