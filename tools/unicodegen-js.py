b236717ad97abfe869da6094483899187cca5654
Michael Homer <mwh@ecs.vuw.ac.nz>
2013-03-08 21:21:46 +1300

    Add JS Unicode data

---
+++ tools/unicodegen-js.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+# Script to generate unicodedata.h from UnicodeData.txt
+# By Michael Homer
+
+import os
+
+data = []
+print("Reading UnicodeData.txt...")
+with open("UnicodeData.txt") as fp:
+    for line in fp:
+        data.append(line.strip().split(";"))
+
+data[-4:] = []
+
+categories = set()
+bidirectionals = set()
+
+codepoints = []
+last = None
+foo = set()
+print("Processing data...")
+for c in data:
+    num = eval("0x" + c[0])
+    if num >= 0x10000:
+        break
+    if c[2] not in categories:
+        categories.add(c[2])
+    if c[4] not in bidirectionals:
+        bidirectionals.add(c[4])
+    if num == 9:
+        c[1] = '<TAB>'
+    while len(codepoints) < num:
+        cp = hex(len(codepoints))[2:]
+        if len(cp) < 4:
+            cp = '0' * (4-len(cp)) + cp
+        foo.add(cp)
+        nxt = [cp, "<A CHARACTER>"]
+        nxt.extend(last[2:])
+        codepoints.append(nxt)
+        last = nxt
+    codepoints.append(c)
+    foo.add(c[0])
+    last = c
+
+
+fp = open("unicodedata.js", "w")
+fp.write("""
+unicodedata = {};
+""")
+
+categories = list(categories)
+cats = dict()
+for i in range(len(categories)):
+    cats[categories[i]] = i
+
+bidirectionals = list(bidirectionals)
+bidis = dict()
+for i in range(len(bidirectionals)):
+    bidis[bidirectionals[i]] = i
+
+print("Generating names map...")
+fp.write("unicodedata.names = {\n")
+for cp in codepoints:
+    if cp[1] == '<A CHARACTER>':
+        continue
+    if cp[1] == '<control>':
+        continue
+    if 'Surrogate' in cp[1]:
+        continue
+    num = eval("0x" + cp[0])
+    char = chr(num)
+    # " or \ or U+2028 LINE SEPARATOR
+    if (char == "\"" or char == "\\" or num == 8232 or num == 0x2029):
+        char = "\\" + char
+    if num >= 0x202a and num <= 0x202e: # LTR modifiers
+        continue
+    fp.write("\"" + char + "\":\"" + cp[1] + "\",\n")
+fp.write("};\n")
+
+fp.write("unicodedata.categories = {};\n")
+i = 1
+for c in categories:
+    print("Generating category " + c + " {}/{}...".format(i, len(categories)))
+    i = i + 1
+    fp.write("unicodedata.categories." + c + " = [")
+    start = -1
+    end = -1
+    for cp in codepoints:
+        if cp[2] == c and start == -1:
+            start = eval("0x" + cp[0])
+        elif start != -1 and cp[2] != c:
+            end = eval("0x" + cp[0]) - 1
+            if start == end:
+                fp.write(str(start) + ", ")
+            else:
+                fp.write("[{}, {}], ".format(start, end))
+            start = -1
+    fp.write("];\n")
+
+fp.write("""
+unicode = {};
+unicode.name = function(s) {
+    if (unicodedata.names[s])
+         return unicodedata.names[s];
+    return "UNICODE CHARACTER " + s.charCodeAt(0);
+};
+unicode.isCategory = function(s, c) {
+    var table = unicodedata.categories[c];
+    var o = s;
+    if (typeof o == "string")
+         o = s.charCodeAt(0);
+    for (var i=0; i<table.length; i++) {
+        var e = table[i];
+        if (typeof e == "number") {
+            if (e == o)
+                return true;
+        } else {
+            if (e[0] <= o && e[1] >= o)
+                return true;
+        }
+    }
+    return false;
+};
+""")
