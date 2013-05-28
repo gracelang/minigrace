75c809af7baadd80938cf4292b24c1d605e533d1
Michael Homer <mwh@ecs.vuw.ac.nz>
2013-05-28 12:47:02 +1200

    Add web worker file to run compiler in background

---
+++ js/background.js
@@ -0,0 +1,39 @@
+// This file provides structure for running the compiler in a
+// background thread with Web Workers. Use it with:
+//   var worker = new Worker("background.js");
+//   worker.onmessage = function(ev) {if (ev.data.success) ...};
+//   worker.postMessage({action: "compile", source: ... });
+// If you are using dialects you will want to use the "import"
+// and "importGCT" actions to load the code back into the compiler
+// thread.
+
+// Some points in minigrace.js use "window" explicitly
+var window = self;
+importScripts("minigrace.js");
+
+var stderr_output = "";
+minigrace.stderr_write = function(value) {
+    stderr_output += value;
+}
+onmessage = function(ev) {
+    var cmd = ev.data;
+    stderr_output = "";
+    if (cmd.action == "compile") {
+        minigrace.modname = cmd.modname;
+        minigrace.mode = cmd.mode;
+        minigrace.compile(cmd.source);
+        if (!minigrace.compileError) {
+            postMessage({success: true, output: minigrace.generated_output,
+                stderr: stderr_output, gct: gctCache[cmd.modname]});
+        } else {
+            postMessage({success: false, stderr: stderr_output});
+        }
+    } else if (cmd.action == "import") {
+        var theModule;
+        eval(cmd.code);
+        eval("theModule = gracecode_" + cmd.modname + ";");
+        self['gracecode_' + cmd.modname] = theModule;
+    } else if (cmd.action == "importGCT") {
+        gctCache[cmd.modname] = cmd.gct;
+    }
+}
