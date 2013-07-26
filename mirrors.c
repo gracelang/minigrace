af50efb6b637db223e9b26c030b753d16470d53c
Andrew P. Black <black@cs.pdx.edu>
2013-07-26 13:13:13 -0700

    Changed comments only, to document the new functions on method mirrors

--- mirrors.c
+++ mirrors.c
@@ -11,9 +11,13 @@
 // Mirrors support two methods:
 //   methods -> List<MirrorMethod>
 //   getMethod(name : String) -> MirrorMethod
-// MirrorMethods support two methods:
+// MirrorMethods support four methods:
 //   name -> String
 //   request(arglists : List of Lists) -> Any
+//   partcount -> Number            the number of parts in the method name
+//   paramcounts -> List of N umbers, of length partcount; the nth
+//                                  number being the number of parameters in the nth parameter list,
+//                                  or zero if there is no such list.
 // A sample use might be:
 //   mirrors.reflect(1).getMethod("+").request([[2]]) == 3
 
