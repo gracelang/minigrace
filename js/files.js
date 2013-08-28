f328c4185843f45f2f63b35b27c151564e24dc51
Michael Homer <mwh@ecs.vuw.ac.nz>
2013-08-28 18:25:18 +1200

    Disable link when createObjectURL unavailable

--- js/files.js
+++ js/files.js
@@ -1,6 +1,8 @@
 var downloadlink = document.getElementById('downloadlink');
 
 function updateDownloadLink() {
+    if (!URL)
+        return;
     if (downloadlink.href)
         URL.revokeObjectURL(downloadlink.href);
     var blob = new Blob([getCode()], {type: "text/x-grace;charset=utf-8"});
