ee38aed64fc25d76df2fddea3bd17117c4a94709
Timothy Jones <git@zmthy.io>
2014-09-05 14:21:39 +1200

    Add definitions to JS modules for objectdraw
    GTK and the timers now register themselves to the editor, and will
    shutdown when stopped, as well as notifying the editor when they end.
    The DOM module now correctly handles booleans and allows Grace code to
    inspect the properties of objects.

--- js/timer.js
+++ js/timer.js
@@ -2,7 +2,7 @@ function gracecode_timer() {
     var o = Grace_allocObject();
     o.methods.intervalList = [];
     o.methods.timeoutList = [];
-    
+
     o.methods.trapErrors = function(func) {
         try {
             func();
@@ -33,80 +33,60 @@ function gracecode_timer() {
             }
         }
     }
-    
-    // the following is part of attempt to halt timers
-    // from different programs then the current one from running
-    if(document.getElementById("timers"))
-        document.getElementById("timers").click();
-    
+
     o.methods["every()do"] = function(argcv, millisec, code) {
-        // check if there's still a stop timers button
-        if(document.getElementById("timers")) {
-            var func = function() {
-                o.methods.trapErrors(function() {
-                    callmethod(code, "apply", [0]);
-                });
-            }
-            var id = setInterval(func, millisec._value);
-            func.id = id;
-            o.methods.intervalList.push(id);
-            return new GraceNum(id);
+        var func = function() {
+            o.methods.trapErrors(function() {
+                callmethod(code, "apply", [0]);
+            });
         }
+        var id = setInterval(func, millisec._value);
+        func.id = id;
+        o.methods.intervalList.push(id);
+        graceRegisterInterval(id);
+        return new GraceNum(id);
     }
-	
+
     o.methods["after()do"] = function(argcv, millisec, code) {
-        // check if there's still a stop timers button
-        if(document.getElementById("timers")) {
-            var func = function() {
-                o.methods.trapErrors(function() {
-                    callmethod(code, "apply", [0]);
-                });
-            }
-            var id = setTimeout(func, millisec._value);
-            func.id = id;
-            o.methods.timeoutList.push(id);
-            return new GraceNum(id);
+        var func = function() {
+            o.methods.trapErrors(function() {
+                callmethod(code, "apply", [0]);
+            });
         }
+        var id = setTimeout(func, millisec._value);
+        func.id = id;
+        o.methods.timeoutList.push(id);
+        graceRegisterTimeout(id);
+        return new GraceNum(id);
     }
-	
+
     o.methods.stop = function(argcv, id) {
-		var index = o.methods.intervalList.indexOf(id._value);
-		if(index != -1) {
+        var index = o.methods.intervalList.indexOf(id._value);
+        if(index != -1) {
             clearInterval(id._value);
-			o.methods.intervalList.splice(index, 1);
-		}
-		var index = o.methods.timeoutList.indexOf(id._value);
+            o.methods.intervalList.splice(index, 1);
+        }
+        var index = o.methods.timeoutList.indexOf(id._value);
         if(index != -1) {
             clearTimeout(id._value);
-			o.methods.timeoutList.splice(index, 1);
-		}
-		
+            o.methods.timeoutList.splice(index, 1);
+        }
+
     }
-	
+
     o.methods.stopAll = function() {
-		var list = o.methods.intervalList;
+        var list = o.methods.intervalList;
         for(var i=0;i<list.length;i++)
             clearInterval(list[i]);
-		
-		o.methods.intervalList = [];
-        
+
+        o.methods.intervalList = [];
+
         list = o.methods.timeoutList;
         for(var i=0;i<list.length;i++)
             clearTimeout(list[i]);
-		
-		o.methods.timeoutList = [];
+
+        o.methods.timeoutList = [];
     }
-    
-    
-    // the following lines are in place to enable the stopping of timers from outside a program
-    if(document.getElementById("extra")) {
-        var stop = document.createElement("button")
-        stop.innerHTML = "Stop Timers"
-        stop.id = "timers";
-        stop.addEventListener("click", o.methods.stopAll, false);
-        document.getElementById("extra").appendChild(stop);
-        stop.onclick = function() {this.parentNode.removeChild(this);}
-    }
-    
+
     return o;
 }
