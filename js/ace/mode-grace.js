40201e7a1bcde30773ad868a39a147b58c54ff04
Andrew P. Black <black@cs.pdx.edu>
2016-02-09 16:10:46 -0800

    Adds keywords to ace-editor mode.
    trait, alias and exclude are added.

--- js/ace/mode-grace.js
+++ js/ace/mode-grace.js
@@ -1023,7 +1023,7 @@ var GraceHighlightRules = function() {
 
     var keywords = lang.arrayToMap(
         (
-            "object|method|factory|self|outer|super|class|def|var|type|return|inherits|is|dialect|import|as"
+            "object|trait|uses|alias|exclude|method|factory|self|outer|super|class|def|var|type|return|inherits|is|dialect|import|as"
         ).split("|")
     );
 
