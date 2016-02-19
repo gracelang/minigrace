gracelang/minigrace

commit 0e1218357fdca26ff1c551c69f94026df85c3b3a

Refactors parsing of object constructors.
Classes, factory methods and objects all have their separate
parsing routines, in spite of the fact that they are the same
syntactic construct.  This introduces a new parsing routine for
objects, and refactors the other three to use it.
It also modifies the AST so that inheritsNodes are stored
_only_ in the superclass and usedTraits fields of the object,
and not also redundantly stored in the body.

identifierresolution.grace
@@ -27,6 +27,9 @@ method newScopeKind(variety') {
     s.hasParent := false
     s
 }
+
+def keyOrdering = { a, b -> a.key.compare(b.key) }
+
 type DeclKind = k.T
 
 factory method newScopeIn(parent') kind(variety') {
@@ -44,7 +47,7 @@ factory method newScopeIn(parent') kind(variety') {
     def hash is public = serialNumber.hash
     
     if (isObjectScope) then {
-        addName "self" as(k.defdec)
+        addName "self" as(k.selfDef)
         at "self" putScope(self)
     }
     method isEmpty { elements.size == 0 }
@@ -67,7 +70,7 @@ factory method newScopeIn(parent') kind(variety') {
         if (kind == k.inherited) then {
             return  // don't overwrite new id with inherited id
         }
-        if (oldKind == k.inherited)  then {
+        if (oldKind.implicit)  then {
             elements.put(ndName, kind)
             elementLines.put(ndName, nd.line)
             return
@@ -114,8 +117,8 @@ factory method newScopeIn(parent') kind(variety') {
         if (elementScopes.contains(n)) then {
             return elementScopes.get(n)
         }
-        //    print("scope {self}: elements.contains({n}) = {elements.contains(n)}" ++
-        //        " but elementScopes.contains({n}) = {elementScopes.contains(n)}")
+        util.log 70 verbose ("scope {self}: elements.contains({n}) = {elements.contains(n)}" ++
+              " but elementScopes.contains({n}) = {elementScopes.contains(n)}")
         //  This occurs for names like `true` that are built-in, but for which there
         //  is no symbolTable describing their atttributes.
         //  TODO: add complete information for the built-in names.
@@ -137,8 +140,8 @@ factory method newScopeIn(parent') kind(variety') {
             result := result ++ each.serialNumber
             if (each.hasParent) then { result := result ++ "➞" }
         }
-        result := result ++  "):\n"
-        elements.asList.sortBy { a, b -> a.key.compare(b.key) }.do { each ->
+        result := result ++  "):\n    "
+        elements.asList.sortBy(keyOrdering).do { each ->
             result := "{result} {each.key}({kind(each.key)}) "
         }
         result ++ "\n"
@@ -149,7 +152,7 @@ factory method newScopeIn(parent') kind(variety') {
     method elementScopesAsString {
         def referencedScopes = set.empty
         var result := "\n    [elementScopes:"
-        elementScopes.asList.sortBy { a, b -> a.key.compare(b.key) }.do { each ->
+        elementScopes.asList.sortBy(keyOrdering).do { each ->
             result := "{result} {each.key}➞{each.value.asDebugString}"
             referencedScopes.add (each.value)
         }
@@ -237,6 +240,7 @@ factory method newScopeIn(parent') kind(variety') {
         // If nd references an object, then the returned
         // scope will have bindings for the methods of that object.
         // Otherwise, it will be the empty scope.
+        util.log 70 verbose "looking for scope referenced by {nd.pretty 0}"
         if (nd.kind == "identifier") then {
             def sought = nd.nameString
             withSurroundingScopesDo {s->
@@ -248,6 +252,7 @@ factory method newScopeIn(parent') kind(variety') {
                 atRange(nd.line, nd.linePos, nd.linePos + sought.size - 1)
         } elseif {nd.kind == "member"} then {
             def receiverScope = self.scopeReferencedBy(nd.in)
+            util.log 70 verbose "receiverScope = {receiverScope}"
             if (nd.value == "outer") then {
                 return receiverScope.parent
             }
@@ -338,6 +343,7 @@ def builtInsScope = newScopeIn(emptyScope) kind("built-in")
 def preludeScope = newScopeIn(builtInsScope) kind("dialect")
 def moduleScope = newScopeIn(preludeScope) kind("module")
 def graceObjectScope = newScopeIn(emptyScope) kind("object")
+def booleanScope = newScopeIn(builtInsScope) kind "object"
 
 def universalScope = object {
     // The scope that defines every identifier,
@@ -692,12 +698,12 @@ method reportUndeclaredIdentifier(node) {
             suggestion.append " do \{ aVarName -> \}" onLine(node.line)
             suggestions.push(suggestion)
         }
-        errormessages.syntaxError "Unknown method '{nm}'. This may be a spelling mistake or an attempt to access a method in another scope.{extra}"
+        errormessages.syntaxError "unknown method '{nm}'. This may be a spelling mistake or an attempt to access a method in another scope.{extra}"
             atRange(node.line, node.linePos, node.linePos +
                 highlightLength - 1)
             withSuggestions(suggestions)
     }
-    errormessages.syntaxError("Unknown variable or method '{nm}'. This may be a spelling mistake or an attempt to access a variable in another scope.")atRange(
+    errormessages.syntaxError("unknown variable or method '{nm}'. This may be a spelling mistake or an attempt to access a variable in another scope.")atRange(
         node.line, node.linePos, node.linePos + highlightLength - 1)withSuggestions(suggestions)
 }
 method reportAssignmentTo(node) declaredInScope(scp) {
@@ -716,13 +722,19 @@ method reportAssignmentTo(node) declaredInScope(scp) {
         if (scp.elementTokens.contains(name)) then {
             def tok = scp.elementTokens.get(name)
             def sugg = errormessages.suggestion.new
-            var eq := tok
-            while {(eq.kind != "op") || (eq.value != "=")} do {
-                eq := eq.next
+            if (tok.value == "def") then {
+                var eq := tok
+                while {(eq.kind != "op") || (eq.value != "=")} do {
+                    eq := eq.next
+                }
+                sugg.replaceToken(eq)with(":=")
+                sugg.replaceToken(tok)with("var")
+                suggestions.push(sugg)
+            } else {
+                errormessages.syntaxError("'{name}' cannot be changed " ++
+                    "because it was declared as a '{tok.value}'{more}.")
+                    atLine(node.line)
             }
-            sugg.replaceToken(eq)with(":=")
-            sugg.replaceToken(tok)with("var")
-            suggestions.push(sugg)
         }
         errormessages.syntaxError("'{name}' cannot be changed "
             ++ "because it was declared with 'def'{more}. "
@@ -773,17 +785,19 @@ method checkForTraitConficts(objNode) {
     // been collected in the object, which happens in the buildSymbolTable
     // visitor pass, so it is convenient to do this check during the
     // resolution pass.
-    def traitMethod = map.new
+    def traitMethods = map.new
     objNode.usedTraits.do { t ->
         t.providedNames.do { methName ->
-            def traitList = traitMethod.get(methName) ifAbsent { [] }
-            traitList.push(t)
-            traitMethod.put(methName, traitList)
+            def definingTraits = traitMethods.get(methName) ifAbsent { [] }
+            definingTraits.push(t)
+            traitMethods.put(methName, definingTraits)
         }
     }
-    traitMethod.keysDo { methName ->
-        def sources = traitMethod.get(methName)
+    traitMethods.keysDo { methName ->
+        def sources = traitMethods.get(methName)
         if (sources.size > 1) then {    // a method has more than one source trait
+            util.log 70 verbose "{objNode.nameString}'s scope = {objNode.scope}"
+            util.log 70 verbose "{objNode.nameString}'s localNames = {objNode.localNames}"
             if (objNode.localNames.contains(methName).not) then {
                 def sourceList = sources.map { s -> s.nameString }
                 // TODO:  clean up these names
@@ -888,12 +902,21 @@ method setupContext(moduleObject) {
     graceObjectScope.addName "asDebugString"
     graceObjectScope.addName "::"
     
+    booleanScope.addName "prefix!"
+    booleanScope.addName "&&"
+    booleanScope.addName "||"
+    booleanScope.addName "andAlso"
+    booleanScope.addName "orElse"
+    booleanScope.addName "not"
+    
     builtInsScope.addName "graceObject"
     builtInsScope.at "graceObject" putScope(graceObjectScope)
     builtInsScope.addName "prelude" as(k.defdec)
     builtInsScope.at "prelude" putScope(preludeScope)
     builtInsScope.addName "_prelude" as(k.defdec)
     builtInsScope.at "_prelude" putScope(preludeScope)
+    builtInsScope.at "true" putScope(booleanScope)
+    builtInsScope.at "false" putScope(booleanScope)
 
     // Historical - should be removed eventually
     if (!util.extensions.contains("NativePrelude")) then {
@@ -1037,20 +1060,9 @@ method buildSymbolTableFor(topNode) ancestors(topChain) {
         method visitInherits(o) up(as) {
             o.scope := as.parent.scope
             if (as.parent.canInherit.not) then {
-                errormessages.syntaxError "{o.statementName} statements must be inside an object or class"
+                errormessages.syntaxError("{o.statementName} statements must " ++
+                    "be inside an object or class; a trait cannot inherit")
                     atRange(o.line, o.linePos, o.linePos + 7)
-            }
-            if (o.isUse) then { 
-                as.parent.usedTraits.add(o)
-            } else {
-                if (as.parent.superclass != false) then {
-                    errormessages.syntaxError ("there can be no more than one inherits " ++
-                        "statement in an object; there was a prior inherits statement " ++
-                        "on line {as.parent.superclass.line}")
-                        atRange(o.line, o.linePos, o.linePos + 7)
-                }
-                as.parent.superclass := o
-                // cache the inherits node in the object or class that contains it
             }
             true
         }
@@ -1123,21 +1135,13 @@ method buildSymbolTableFor(topNode) ancestors(topChain) {
         method visitCommentNode(o) up(as) { o.scope := as.parent.scope ; true }
     }   // end of symbolTableVis
     
+    def objectScopesVis = object {
+        // This traversal can't be completed in the buildSymbolTable visitor,
+        // because the visitation is top-down, and hence the scope of the
+        // dody of a def or method won't have been allocated when the 
+        // delcaration is visited.
 
-    def inheritanceVis = object {
         inherits ast.baseVisitor
-        method visitClass(o) up (as) {
-            collectInheritedNames(o)
-            true
-        }
-        method visitObject(o) up (as) {
-            collectInheritedNames(o)
-            true
-        }
-        method visitModule(o) up (as) {
-            collectInheritedNames(o)
-            true
-        }
         method visitDefDec(o) up (as) {
             if (o.returnsObject) then {
                 o.scope.at(o.nameString)
@@ -1154,7 +1158,24 @@ method buildSymbolTableFor(topNode) ancestors(topChain) {
         }
     }
 
+    def inheritanceVis = object {
+        inherits ast.baseVisitor
+        method visitClass(o) up (as) {
+            collectInheritedNames(o)
+            true
+        }
+        method visitObject(o) up (as) {
+            collectInheritedNames(o)
+            true
+        }
+        method visitModule(o) up (as) {
+            collectInheritedNames(o)
+            true
+        }
+    }
+
     topNode.accept(symbolTableVis) from(topChain)
+    topNode.accept(objectScopesVis) from(topChain)
     topNode.accept(inheritanceVis) from(topChain)
 }
 
@@ -1162,6 +1183,9 @@ method collectInheritedNames(node) {
     // node is an object or class; put the names that it inherits into its scope.
     // In the process, checks for a cycle in the inheritance chain.
     def nodeScope = node.scope
+    if (nodeScope == ast.fakeSymbolTable) then {
+        util.log 20 verbose "node {node} has no scope.\n{node.pretty 0}"
+    }
     if (nodeScope.inheritedNames == completed) then { 
         return
     }
@@ -1182,7 +1206,11 @@ method collectInheritedNames(node) {
                 // superScope.node == nullNode when superScope describes 
                 // an imported module.
                 collectInheritedNames(superScope.node)
+            } else {
+                util.log 70 verbose "‹{node.nameString}›.superscope.node == nullNode"
             }
+        } else {
+            util.log 70 verbose "superscope of {node.nameString} is universal"
         }
     }
     superScope.elements.keysDo { each ->

ast.grace
@@ -1387,7 +1387,7 @@ def moduleNode = object {
         }
     }
 }
-def objectNode = object {
+def objectNode is public = object {
     method body(b) named(n) scope(s) {
         def result = new(b, false)
         result.name := n
@@ -1398,21 +1398,22 @@ def objectNode = object {
         body(b) named(n) scope(fakeSymbolTable)
     }
     class new(b, superclass') {
-        // TODO  remove superclass as a parameter
         inherits baseNode
         def kind is public = "object"
         var value is public := b
         var superclass is public := superclass'
         var usedTraits is public := list.empty
-        var name is public := "object"
+        var name:String is public := "object"
         var inClass is public := false
         var inTrait is public := false
         var myLocalNames := false
+        var annotations is public := list.empty
 
         method isTrait {
             // answers true if this object qualifies to be a trait, whether
             // or not it was declared with the trait syntax
             if (inTrait) then { return true }
+            if (false != superclass) then { return false }
             value.do { each -> 
                 if (each.isLegalInTrait.not) then { return false }
             }
@@ -1423,13 +1424,17 @@ def objectNode = object {
             // answers the names of all of the methods of this object, including
             // those inherited or obtained by using a trait
             def result = set.empty
+            if (false != superclass) then { 
+                result.addAll(superclass.providedNames)
+            }
+            usedTraits.do { each ->
+                result.addAll(each.providedNames)
+            }
             value.do {each ->
                 if (each.isMethod) then { 
                     result.add(each.nameString)
                 } elseif { each.isTypeDec } then {
                     result.add(each.nameString)
-                } elseif { each.isInherits } then { 
-                    result.addAll(each.providedNames) 
                 }
             }
             result
@@ -1441,7 +1446,7 @@ def objectNode = object {
             if (false == myLocalNames) then {
                 myLocalNames := set.empty
                 scope.keysAndKindsDo { methName, knd ->
-                    if (knd != k.inherited) then {
+                    if (knd.isImplicit.not) then {
                         myLocalNames.add(methName)
                     }
                 }
@@ -1463,12 +1468,11 @@ def objectNode = object {
         method accept(visitor : ASTVisitor) from(as) {
             if (visitor.visitObject(self) up(as)) then {
                 def newChain = as.extend(self)
-                if (self.superclass != false) then {
-                    self.superclass.accept(visitor) from(newChain)
+                if (superclass != false) then {
+                    superclass.accept(visitor) from(newChain)
                 }
-                for (self.value) do { x ->
-                    x.accept(visitor) from(newChain)
-                }
+                usedTraits.do { t -> t.accept(visitor) from(newChain) }
+                value.do { x -> x.accept(visitor) from(newChain) }
             }
         }
         method nameString { 
@@ -1497,13 +1501,17 @@ def objectNode = object {
             if (inTrait) then { s := s ++ " (trait)" }
             if (inClass) then { s := s ++ " (class)" }
             if (self.superclass != false) then {
-                s := s ++ "\n" ++ spc ++ "Superclass:"
-                s := s ++ "\n  " ++ spc ++ self.superclass.pretty(depth + 1)
-                s := s ++ "\n" ++ spc ++ "Body:"
-                depth := depth + 1
+                s := s ++ "\n" ++ spc ++ "Superclass: " ++ 
+                        self.superclass.pretty(depth + 1)
             }
-            for (self.value) do { x ->
-                s := s ++ "\n"++ spc ++ x.pretty(depth+1)
+            if (usedTraits.isEmpty.not) then {
+                s := s ++ "\n" ++ spc ++ "Traits:"
+                usedTraits.do { t -> 
+                    s := "{s}\n{spc}  {t.pretty(depth + 1)}"
+                }
+            }
+            value.do { x ->
+                s := s ++ "\n"++ spc ++ x.pretty(depth + 1)
             }
             s
         }
@@ -1515,7 +1523,11 @@ def objectNode = object {
             var s := "object \{"
             if (inTrait) then { s := s ++ "   // trait" }
             if (inClass) then { s := s ++ "   // class" }
-            for (self.value) do { x ->
+            if (false != superclass) then { 
+                s := s ++ "\n" ++ superclass.toGrace(depth + 1)
+            }
+            usedTraits.do { t -> s := s ++ "\n" ++ t.toGrace(depth + 1) }
+            value.do { x ->
                 s := s ++ "\n" ++ spc ++ "    " ++ x.toGrace(depth + 1)
             }
             s := s ++ "\n" ++ spc ++ "\}"
@@ -1532,6 +1544,7 @@ def objectNode = object {
             usedTraits := other.usedTraits
             inClass := other.inClass
             inTrait := other.inTrait
+            annotations := other.annotations
             self
         }
         method asString {
@@ -1653,7 +1666,10 @@ def memberNode = object {
         method asString { "{in}.{value}" }
         method asIdentifier {
             // make and return an identifiderNode for my request
-            def resultNode = identifierNode.new(value, false)
+            if (scope == fakeSymbolTable) then {
+                ProgrammingError.raise "asIdentifier requested on {pretty 0} when scope was fake"
+            }
+            def resultNode = identifierNode.new (value, false) scope (scope)
             resultNode.inRequest := true
             resultNode.line := line
             resultNode.linePos := linePos
@@ -2000,7 +2016,7 @@ def opNode is public = object {
     }
     method asIdentifier {
         // make an identifiderNode with the same properties as me
-        def resultNode = identifierNode.new(value, false)
+        def resultNode = identifierNode.new (value, false) scope (scope)
         resultNode.inRequest := true
         resultNode.line := line
         resultNode.linePos := linePos
@@ -2157,9 +2173,7 @@ def defDecNode = object {
                 for (self.annotations) do { ann ->
                     ann.accept(visitor) from(newChain)
                 }
-                if (self.value != false) then {
-                    self.value.accept(visitor) from(newChain)
-                }
+                value.accept(visitor) from(newChain)
             }
         }
         method map(blk) ancestors(as) {

genc.grace
@@ -499,25 +499,30 @@ method compileobject(o, outerRef) {
         if (e.kind == "method") then {
         } elseif { e.kind == "vardec" } then {
             compileobjvardecdata(e, selfr, pos)
+            pos := pos + 1
         } elseif { e.kind == "defdec" } then {
             compileobjdefdecdata(e, selfr, pos)
+            pos := pos + 1
         } elseif { e.kind == "typedec" } then {
             compileobjdefdecdata(e, selfr, pos)
+            pos := pos + 1
         } elseif { e.kind == "class" } then {
-        } elseif { e.kind == "inherits" } then {
-            // The return value is irrelevant with factory inheritance,
-            // but we save it as super for the sake of "inherits true".
-            superobj := compilenode(e.value)
-            out "  struct UserObject *{selfr}_uo = (struct UserObject *){selfr};"
-            out "  {selfr}_uo->super = {superobj};"
-            implementAliasesAndExclusionsFor(o) inheriting(e, superobj)
-            pos := pos - 1
         } else {
             compilenode(e)
-            pos := pos - 1 // Compensate for below
         }
-        pos := pos + 1
     }
+    
+    // compile inheritance
+    
+    if (false != o.superclass) then {
+        superobj := compilenode(o.superclass.value)
+        out "  struct UserObject *{selfr}_uo = (struct UserObject *){selfr};"
+        out "  {selfr}_uo->super = {superobj};"
+        implementAliasesAndExclusionsFor(o) inheriting(o.superclass, superobj)
+    }
+    
+    // TODO: compile traits
+
     out("  objclass{myc} = {selfr}->class;")
     out("  objclass{myc}->definitionModule = modulename;")
     out("  objclass{myc}->definitionLine = {o.line};")

genjs.grace
@@ -367,14 +367,18 @@ method compileobject(o, outerRef, inheritingObject) {
         } elseif { e.kind == "object" } then {
             out "sourceObject = {selfr};"
             compileobject(e, selfr, false)
-        } elseif { e.kind == "inherits" } then {
-            out "sourceObject = {selfr};"
-            compileInherits(e, selfr)
         } else {
             out "sourceObject = {selfr};"
             compilenode(e)
         }
     }
+    
+    // compile inherits
+    if (false != o.superclass) then {
+        out "sourceObject = {selfr};"
+        compileInherits(o.superclass, selfr)
+    }
+
     out "superDepth = origSuperDepth;"
     decreaseindent
     out "\};"

identifierKinds.grace
@@ -1,21 +1,35 @@
 // Constants distinguishing between different kinds of identifier.
+// Defines observers for their properties.
 
 type T = type {
     isParameter -> Boolean
     isAssignable -> Boolean
+    isImplicit -> Boolean
 }
 
 class kindConstant(name) {
     method asString { name }
     method isParameter { false }
     method isAssignable { false }
+    method isImplicit { false }
 }
 
+def undefined = kindConstant "undefined"
 def defdec = kindConstant "defdec"
 def methdec = kindConstant "method"
 def typedec = kindConstant "typedec"
-def inherited = kindConstant "inherited"
-def undefined = kindConstant "undefined"
+def selfDef = object {
+    inherits kindConstant "selfDef"
+    method isImplicit { true }
+}
+def fromTrait = object {
+    inherits kindConstant "fromTrait"
+    method isImplicit { true }
+}
+def inherited = object {
+    inherits kindConstant "inherited"
+    method isImplicit { true }
+}
 def vardec = object {
     inherits kindConstant "vardec"
     method isAssignable { true }

commit 1f359dcc5223d9c9b00f39cb78025e34aaf29a7c

Restores error message on assignment to self.
This was broken in 0e1218357fdc when `self` was given a
distinguished identifierKind.

Also replaces blanket line bumbers by accurate ranges in error message.

identifierresolution.grace
@@ -707,6 +707,8 @@ method reportUndeclaredIdentifier(node) {
         node.line, node.linePos, node.linePos + highlightLength - 1)withSuggestions(suggestions)
 }
 method reportAssignmentTo(node) declaredInScope(scp) {
+    // Report a syntax error for an illegal assignment
+
     def name = node.nameString
     def kind = scp.kind(name)
     var more := ""
@@ -714,11 +716,15 @@ method reportAssignmentTo(node) declaredInScope(scp) {
     if (scp.elementLines.contains(name)) then {
         more := " on line {scp.elementLines.get(name)}"
     }
-    if (kind == k.defdec) then {
-        if (reserved.contains(name)) then {
-            errormessages.syntaxError("'{name}' is a reserved name and "
-                ++ "cannot be re-bound.") atLine(node.line)
-        }
+    if (kind == k.selfDef) then {
+        errormessages.syntaxError("'{name}' cannot be re-bound; " ++
+              "it always refers to the current object.")
+              atRange(node.line, node.linePos, node.linePos + name.size - 1)
+    } elseif { reserved.contains(name) } then {
+        errormessages.syntaxError("'{name}' is a reserved name and " ++
+              "cannot be re-bound.")
+              atRange(node.line, node.linePos, node.linePos + name.size - 1)
+    } elseif { kind == k.defdec } then {
         if (scp.elementTokens.contains(name)) then {
             def tok = scp.elementTokens.get(name)
             def sugg = errormessages.suggestion.new
@@ -733,25 +739,26 @@ method reportAssignmentTo(node) declaredInScope(scp) {
             } else {
                 errormessages.syntaxError("'{name}' cannot be changed " ++
                     "because it was declared as a '{tok.value}'{more}.")
-                    atLine(node.line)
+                    atRange(node.line, node.linePos, node.linePos + name.size - 1)
             }
         }
         errormessages.syntaxError("'{name}' cannot be changed "
             ++ "because it was declared with 'def'{more}. "
             ++ "To make it a variable, use 'var' in the declaration")
-            atLine(node.line) withSuggestions(suggestions)
+            atRange(node.line, node.linePos, node.linePos + name.size - 1)
+            withSuggestions(suggestions)
     } elseif { kind == k.typedec } then {
         errormessages.syntaxError("'{name}' cannot be re-bound "
             ++ "because it is declared as a type{more}.")
-            atLine(node.line)
+            atRange(node.line, node.linePos, node.linePos + name.size - 1)
     } elseif { kind.isParameter } then {
         errormessages.syntaxError("'{name}' cannot be re-bound "
             ++ "because it is declared as a parameter{more}.")
-            atLine(node.line)
+            atRange(node.line, node.linePos, node.linePos + name.size - 1)
     } elseif { kind == k.methdec } then {
         errormessages.syntaxError("'{name}' cannot be re-bound "
             ++ "because it is declared as a method{more}.")
-            atLine(node.line)
+            atRange(node.line, node.linePos, node.linePos + name.size - 1)
     }
 }
 