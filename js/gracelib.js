27fd2049bda4a5b7f84548b81ff297794854b357
Andrew P. Black <black@cs.pdx.edu>
2016-09-07 15:38:37 -0700

    Defines Alias as a global for node.
    That's all

--- js/gracelib.js
+++ js/gracelib.js
@@ -3760,6 +3760,7 @@ var var_done = GraceDone;
 
 // for node: explicitly make names global
 if (typeof global !== "undefined") {
+    global.Alias = Alias;
     global.callmethod = callmethod;
     global.callmethodChecked = callmethodChecked;
     global.callmethodsuper = callmethodsuper;
