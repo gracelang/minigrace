gracelang/minigrace

commit 23d441aeda3f30e5aa5089f657e7523b4a54e748

Checks for module names ending in .grace
Resolves issue #248.  Adds test.

ast.grace
@@ -2814,9 +2814,9 @@ def importNode is public = object {
   }
 }
 def dialectNode is public = object {
-  method fromToken(stringTok) {
-    def result = new(stringTok.value)
-    result.end := line (stringTok.line) column (stringTok.linePos + stringTok.size - 1)
+  method fromStringNode(s) {
+    def result = new(s.value)
+    result.end := line (s.line) column (s.end)
     result
   }
   class new(pathString) {

js/tests/t179_minitest_compiler_test.grace
@@ -190,3 +190,19 @@ testSuiteNamed "RHS of typedec #289" with {
               mentioning "a type declaration must have a type expression"
     }
 }
+
+testSuite "module name ending with .grace #248" with {
+    test "import wih .grace" by {
+        assert { astNode "import" from ‹import "mirror.grace" as m› }
+              shouldRaise (em.SyntaxError)
+              mentioning "import" and "must not end with \".grace\""
+    }
+    test "dialect wih .grace" by {
+        assert { astNode "dialect" from ‹dialect "objectdraw.grace"› }
+              shouldRaise (em.SyntaxError)
+              mentioning "dialect" and "must not end with \".grace\""
+    }
+}
+
+exit
+

parser.grace
@@ -2086,8 +2086,13 @@ method dodialect {
                 "of the dialect in quotes after the word 'dialect'.")
                 atPosition(lastToken.line, errorPos)withSuggestion(suggestion)
         }
-        def dn = ast.dialectNode.fromToken(sym).setPositionFrom(dialectToken)
-        next
+        pushString
+        def p = values.pop
+        def dn = ast.dialectNode.fromStringNode(p).setPositionFrom(dialectToken)
+        if (dn.value.endsWith ".grace") then {
+            errormessages.syntaxError "the name of the dialect must not end with \".grace\""
+                  atRange(p)
+        }
         if (values.isEmpty) then {
             if (moduleObject.theDialect.line == 0) then {
                 moduleObject.theDialect := dn
@@ -2703,6 +2708,10 @@ method doimport {
         }
         pushString
         def p = values.pop
+        if (p.value.endsWith ".grace") then {
+            errormessages.syntaxError("the name of the imported module must not end with \".grace\"")
+                atRange(p)
+        }
         if (! acceptKeyword "as") then {
             var suggestion := errormessages.suggestion.new
             if ((sym.isIdentifier) && (sym.line == lastToken.line)) then {

commit 82c50efd015504981a218c6d4c507f54c75ccd4d

Fixes bug introduced in 23d441a
The end of an AstNode is a position, not a number.

ast.grace
@@ -2815,7 +2815,7 @@ def importNode is public = object {
 def dialectNode is public = object {
   method fromStringNode(s) {
     def result = new(s.value)
-    result.end := line (s.line) column (s.end)
+    result.end := s.end
     result
   }
   class new(pathString) {